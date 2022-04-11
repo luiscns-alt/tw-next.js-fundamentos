83ee6429dd30348125c0afcc88fcef5663c7af4d
Luis Carlos <luiscarlosnors@gmail.com>
2022-04-10 23:51:49 -0300

    Navegação e Pré:Carregamento

--- web/src/pages/contatos.tsx
+++ web/src/pages/contatos.tsx
@@ -1,7 +1,10 @@
+import Link from 'next/link';
+
 export default function Contatos() {
     return (
         <>
             <h1>Contatos</h1>
+            <Link href='/'>Home</Link>
         </>
     );
 }
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
     return (
         <div>
             <h1>Home</h1>
+            <Link href='/contatos'>Contatos</Link>
         </div>
     );
 };
